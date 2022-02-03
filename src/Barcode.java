7518bb6b6bcd87f63c6b30940a66bde629616c40
unknown <Monga@1WP1186-LAP-01.business.rutgers.edu>
2022-02-03 09:18:08 -0500

    wrapped up most methods except binaryConverter (restart this)

--- src/Barcode.java
+++ src/Barcode.java
@@ -3,52 +3,88 @@ import java.util.Scanner;
 public class Barcode {
 
     public static void main(String[] args) {
-        System.out.println(initialBarcodeConverter556s("010011"));
-        /*
+        System.out.println(binaryConverter(5));
+    }
+
+    public static void run() {
         System.out.println("Welcome to Nirayka's Postal- to Bar-Code Converter!");
         boolean entering = true;
-        while (true) {
-            System.out.println("Please enter the postal code that you'd like to convert to a barcode.");
+        while (entering) {
+            System.out.println("Please type in the postal code that you'd like to convert, and hit enter.");
             Scanner scan = new Scanner(System.in);
-            int postalCode = scan.nextInt();
-            int length = String.valueOf(postalCode);
-            if (length > 5 || length < 5) {
-
-            } */
-        }
-
-        public static String binaryConverter(int decimal) {
-            return(Integer.toBinaryString(decimal));
-        }
-
-        public static String initialBarcodeConverter(String binary) {
-            String initialBarcode = "";
-            for (int i = 0; i < 5; i++) {
-                String bar = "";
-                char thisDigitChar = binary.charAt(i);
-                String thisDigit = String.valueOf(thisDigitChar);
-
-                if (thisDigit.equals("0")) {
-                    bar = ":";
-                } else if (thisDigit.equals("1")) {
-                    bar = "|";
-                }
-                initialBarcode += bar;
+            String postalString = scan.nextLine();
+            if (isPostalCodeValid(postalString)) {
+                int postalInt = Integer.parseInt(postalString);
+                runIndividual(postalInt);
+            } else if (postalString.toLowerCase().equals("esc")) {
+                System.out.println("You have chosen to exit the program.");
+                entering = false;
+            } else {
+                System.out.println("You have entered an invalid option.");
             }
-            return initialBarcode;
         }
-    public static String initialBarcodeConverter2(String binary) {
+    }
+
+    public static boolean isPostalCodeValid(String postalCode) {
+        if (isNumeric(postalCode)) {
+            if (postalCode.length() == 5) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean isNumeric(String postalCode) {
+        if (postalCode == null) {
+            return false;
+        }
+        try {
+            int thisInt = Integer.parseInt(postalCode);
+        } catch (NumberFormatException nfe) {
+            return false;
+        }
+        return true;
+    }
+
+    public static void runIndividual(int postalInt) {
+
+    }
+
+
+    public static String binaryConverter(int input) { // got to fix this, must convert EACH INDIVIDUAL DIGIT to binary
+        
+    }
+
+
+    public static String initialBarcodeConverter(String binary) {
         String initialBarcode = "";
+        for (int i = 0; i < binary.length(); i++) {
             String bar = "";
-            char thisDigitChar = binary.charAt(1);
+            char thisDigitChar = binary.charAt(i);
             String thisDigit = String.valueOf(thisDigitChar);
-        if (thisDigit.equals("0")) {
-            bar = ":";
-        } else if (thisDigit.equals("1")) {
-            bar = "|";
+            if (thisDigit.equals("0")) {
+                bar = ":";
+            } else if (thisDigit.equals("1")) {
+                bar = "|";
+            }
+            initialBarcode += bar;
         }
-        return bar;
+        return initialBarcode;
+    }
+
+    public static String addCorrectionDigit(int postalInt) {
+        String correctionBars = "";
+        int sum = 0;
+        while (postalInt != 0) {
+            sum = sum + postalInt % 10;
+            postalInt = postalInt / 10;
+        }
+        int remainder = sum % 10;
+        if (remainder != 0) {
+            correctionBars += initialBarcodeConverter(binaryConverter(remainder));
+        }
+        return correctionBars;
     }
 
 
-    }
+}
